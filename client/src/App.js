40dfed86ead2c8ac45f6ebba542ba6041ecb6339
ZQWhiting <zach.whiting@icloud.com>
2020-10-03 12:59:18 -0600

    created ApolloProvider and GlobalStore; Added stores and router to App.js; modified mongodb connection string

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
-function App() {}
+import { Provider } from 'react-redux';
+import store from './utils/State';
+
+const client = new ApolloClient({
+	request: (operation) => {
+		const token = localStorage.getItem('id_token');
+		operation.setContext({
+			headers: {
+				authorization: token ? `Bearer ${token}` : '',
+			},
+		});
+	},
+	uri: '/graphql',
+});
+
+function App() {
+	return (
+		<ApolloProvider client={client}>
+			<Provider store={store}>
+				<Router>
+					{/* Header/Nav */}
+					<Switch>{/* <Route exact path={} component={} />*/}</Switch>
+					{/* Footer */}
+				</Router>
+			</Provider>
+		</ApolloProvider>
+	);
+}
 
 export default App;
