e0e97a2ecc12b96784731df346457dfa57844127
ZQWhiting <zach.whiting@icloud.com>
2020-10-06 13:56:01 -0600

    created framework for homepage

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,18 @@ import client from './utils/Apollo';
 import { Provider } from 'react-redux';
 import store from './utils/State';
 import Book from './components/Book';
-  
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 function App() {
 	return (
 		<ApolloProvider client={client}>
 			<Provider store={store}>
 				<Router>
-            <div>
-              <h1> Book Forum</h1>
-              <div className= "container">
-                  <Book /> 
-              </div>
-            </div>
 					{/* Header/Nav */}
 					<Switch>
-						{/* <Route exact path='/' component={Home} /> */}
-						{/* <Route component={NoMatch} /> */}
+						<Route exact path='/' component={Home} />
+						<Route component={NotFound} />
 					</Switch>
 					{/* Footer */}
 				</Router>
