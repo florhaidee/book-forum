6c1da1b01e3c3db2fbec8df32028fe3642111531
ZQWhiting <zach.whiting@icloud.com>
2020-10-06 13:38:04 -0600

    created Header framework

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,17 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import client from './utils/Apollo';
 import { Provider } from 'react-redux';
 import store from './utils/State';
+
 import Book from './components/Book';
-  
+import Header from './components/Header';
+
 function App() {
 	return (
 		<ApolloProvider client={client}>
 			<Provider store={store}>
 				<Router>
-            <div>
-              <h1> Book Forum</h1>
-              <div className= "container">
-                  <Book /> 
-              </div>
-            </div>
-					{/* Header/Nav */}
+					<Header />
+                    <Book />
 					<Switch>
 						{/* <Route exact path='/' component={Home} /> */}
 						{/* <Route component={NoMatch} /> */}
