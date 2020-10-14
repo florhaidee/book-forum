83987afd40a60f0a30c9150cf743629506c5d7ae
jeremyrice98 <ricefamily1003@gmail.com>
2020-10-13 21:23:32 -0600

    add picture links to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import Book from '../components/Book';
+import {Link} from 'react-router-dom'
+import '../index.css'
+
 
 const Home = () => {
 	return (
 		<main>
-			<div id='genreGrid'>
-				<Book />
-			</div>
+			<div class="container-fluid">
+  
+    <div class="col">
+	<Link to="/threads/fantasy">  
+<img src="images/fantasy-book.jpg"/>
+	
+	</Link> 
+    </div>
+
+	<div class="col">
+	<Link to="/threads/adventure">  
+	<img src="images/adventure.jpg" />
+	</Link> 
+    </div>
+	
+
+    <div class="col">
+	<Link to="/threads/romance">  
+	<img src="images/heart-book.jpg" />
+	</Link> 
+    </div>
+	<div class="col">
+	<Link to="/threads/mystery">  
+	<img src="images/mystery-book.png" />
+	</Link> 
+    </div>
+	</div>
+			
 		</main>
 	);
 };
