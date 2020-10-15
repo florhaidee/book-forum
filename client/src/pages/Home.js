56e9d8712ca6feb26c5b3e4b98adaaa6e97ff0f5
ZQWhiting <zach.whiting@icloud.com>
2020-10-15 17:56:54 -0600

    fixed homepage styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,28 +7,30 @@ import '../index.css'
 
 const Home = () => {
 	return (
-		<main>
-			<div className="container-fluid">
+		<main className='container'>
+			<div className="row homepage">
 				{/*Fantasy*/}
-				<div class="col">
+				<div class="col-12 col-md-6 col-lg-3">
 					<Link to="/threads/Fantasy">
 					<img src="images/fantasy-book.jpg"/>
 					</Link>
+					<span>Fantasy</span>
 				</div>
 				{/*Adventure*/}
-				<div class="col">
+				<div class="col-12 col-md-6 col-lg-3">
 					<Link to="/threads/Adventure">
 					<img src="images/adventure.jpg" />
 					</Link>
 				</div>
 				{/*Romance*/}
-				<div class="col">
+				<div class="col-12 col-md-6 col-lg-3">
 					<Link to="/threads/Romance">
 					<img src="images/heart-book.jpg" />
 					</Link>
+					<span>Romance</span>
 				</div>
 				{/*Mystery*/}
-				<div class="col">
+				<div class="col-12 col-md-6 col-lg-3">
 					<Link to="/threads/Mystery">
 					<img src="images/mystery-book.png" />
 					</Link>
