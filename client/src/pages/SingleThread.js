02deb2a22cc1e8df079f707085b0f484c3a5338d
Florhaidee <florhaideedev@gmail.com>
2020-10-15 18:14:14 -0600

    added new single page

--- client/src/pages/SingleThread.js
+++ client/src/pages/SingleThread.js
@@ -1,29 +1,53 @@
 import React from 'react';
-//import { useLazyQuery } from '@apollo/client';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_THREADS } from '../utils/queries';
-import { useSelector } from 'react-redux';
-import Book from '../components/Book';
+import { QUERY_THREAD } from '../utils/queries';
+import HTMLFlipBook from "react-pageflip";
 
-
+const Page = React.forwardRef((props, ref) => {
+	return (
+		<div className='Page' ref={ref}>
+			<h2 className='pageHeader'>{props.children.genre}</h2>
+			<div className='pageText'>
+				{props.children.threadText}
+				{props.children.posts.length > 0 ? (
+					<h4>Posts: </h4>
+				) : (
+					<h5>No Posts Yet. </h5>
+				)}
+				{props.children.posts.length > 0 &&
+					props.children.posts.map((post) => {
+						return <p className='postText'>{post.postBody}</p>;
+					})}
+			</div>
+      <Link to={`/threads/${props.children.genre}`}>See All Threads!</Link>
+			<div className='pageFooter'>
+				Page number: {props.number + 2} - {props.children.createdAt} -
+				Created by: {props.children.username}
+			</div>
+		</div>
+	);
+});
 
 const SingleThread = props => {
 
-  // const state = useSelector((state) => state.thread);
-  // const { loading, data } = useQuery(QUERY_THREADS, {
-  //   variables: state ,
-  // });
-  // const threads = data?.threads || [];
+  let { id: id} = useParams();
+  const { loading, data } = useQuery(QUERY_THREAD, {
+    variables: { _id: id},
+  });
 
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+  const thread = data?.thread || [];
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div>
-      <Book/>
-    </div>
+      <div className="SinglePage">
+        <HTMLFlipBook width={700} height={800}>
+          <Page number={1} key={thread._id}>{ thread }</Page>
+        </HTMLFlipBook>
+      </div>
   );
 };
 
---
+++ client/src/pages/Threads.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Book from '../components/Book';
+
+
+
+const Threads = props => {
+
+  return (
+    <div>
+      <Book/>
+    </div>
+  );
+};
+
+
+export default Threads;
